8dc275865441c7dfabccd970765657eb121aa77a
songtengqi <songtengqi@gmail.com>
2022-03-07 15:45:23 -0500

    'add_a_demo_file'

---
+++ GitDemo.java
@@ -0,0 +1,13 @@
+public class demo{
+    private Integer id;
+    public demo(){
+        id = 10;
+    }
+
+    @Override
+    public String toString() {
+        return "demo{" +
+                "id=" + id +
+                '}';
+    }
+}
